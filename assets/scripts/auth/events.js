d17228595ac3314fd8adc244544697d563b39317
laurenallegra <lauren.allegra@gmail.com>
2016-10-15 22:33:57 -0400

    Add full User Authentication
    Allow user to sign up, sign in, change password, and sign out using
    POST, POST, PATCH, and DELETE ajax requests, respectively.

--- assets/scripts/auth/events.js
+++ assets/scripts/auth/events.js
@@ -5,8 +5,38 @@ const getFormFields = require(`../../../lib/get-form-fields`);
 const api = require('./api');
 const ui = require('./ui');
 
+const onSignUp = function (event) {
+  event.preventDefault();
+  let data = getFormFields(event.target);
+  api.signUp(data)
+    .done(ui.signUpSuccess)
+    .fail(ui.signFailure);
+};
+const onSignIn = function (event) {
+  event.preventDefault();
+  let data = getFormFields(event.target);
+  api.signIn(data)
+    .done(ui.signInSuccess)
+    .fail(ui.signFailure);
+};
+const onChangePassword = function (event) {
+  event.preventDefault();
+  let data = getFormFields(event.target);
+  api.changePassword(data)
+    .done(ui.changePasswordSuccess)
+    .fail(ui.failure);
+};
+const onSignOut = function (event) {
+  event.preventDefault();
+  api.signOut()
+    .done(ui.signOutSuccess)
+    .fail(ui.failure);
+};
 const addHandlers = () => {
-
+  $('#sign-up').on('submit', onSignUp);
+  $('#sign-in').on('submit', onSignIn);
+  $('#change-password').on('submit', onChangePassword);
+  $('#sign-out').on('submit', onSignOut);
 };
 
 module.exports = {
