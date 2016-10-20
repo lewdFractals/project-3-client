5b254497e346727d100ef983b089a1d3d0e12f02
laurenallegra <lauren.allegra@gmail.com>
2016-10-19 23:55:05 -0400

    Remove bundle.js and vendor.js from .gitignore

--- vendor.bundle.js
+++ vendor.bundle.js
@@ -100,7 +100,7 @@
 /***/ function(module, exports, __webpack_require__) {
 
 	__webpack_require__(2);
-	module.exports = __webpack_require__(23);
+	module.exports = __webpack_require__(52);
 
 
 /***/ },
@@ -9935,7 +9935,7 @@
 
 /***/ },
 
-/***/ 23:
+/***/ 52:
 /***/ function(module, exports, __webpack_require__) {
 
 	/* WEBPACK VAR INJECTION */(function(jQuery) {/*!
